89eb5ac04622d235989a71761aad9cb0532e4e2e
Aashika <khatiwadaaashika12@gmail.com>
2026-01-25 13:47:17 +0545

    stringbuilder

---
+++ stringbuilder/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb= new StringBuilder("Hello");
+       
+        sb.append(" Aashika"); //adds
+        System.out.println(sb);
+
+        sb.replace(0,5,"Hi"); //replaces from 0 to 4
+        System.out.println(sb);
+
+        sb.append(" Friend");
+        System.out.println(sb);
+
+        sb.delete(10,1);
+        System.out.println(sb);
+
+        sb.insert(3,"Miss.");
+        System.out.println(sb);
+    }
+}
