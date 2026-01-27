8484ce875b97b79c348f199b324a1cd848cb6d46
Aashika <khatiwadaaashika12@gmail.com>
2026-01-27 18:45:13 +0545

    interface

--- paymentsystem_interface/src/App.java
+++ paymentsystem_interface/src/App.java
@@ -3,3 +3,21 @@ public class App {
         System.out.println("Hello, World!");
     }
 }
+interface Payment{
+    void pay(double amt);
+ }
+class Esewa implements Payment{
+    public void pay(double amt){
+        System.out.println("User payed Rs."+amt+" using Esewa");
+    }
+}
+class Khalti implements Payment{
+    public void pay(double amt){
+        System.out.println("User payed Rs."+amt+" using Khalti");
+    }
+}
+class Cash implements Payment{
+    public void pay(double amt){
+        System.out.println("User payed Rs."+amt+" using Khalti");
+    }
+}
