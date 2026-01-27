13ab0e2cf74b61f1c899213cccdbb7a09b3b7f18
Aashika <khatiwadaaashika12@gmail.com>
2026-01-27 14:46:13 +0545

    method overriding

--- runtimePolymorphism/src/App.java
+++ runtimePolymorphism/src/App.java
@@ -2,7 +2,8 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Shape s= new Circle();
+        s.draw();
     }
 }
 
