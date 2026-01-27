d0898f8b711fd91c2da10977f2177d5c50aa0977
Aashika <khatiwadaaashika12@gmail.com>
2026-01-27 11:03:58 +0545

    java

--- student_profile_manager/src/App.java
+++ student_profile_manager/src/App.java
@@ -1,10 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       
+       Student std= new Student("Anna",98);
+       std.marks=97; //changed the alue of marks
+       Student std2= new Student(std); //obj for copy constructor orr oj initialied by copy constructor
+       System.out.println("Student "+std.stdName+" got "+std.marks+" marks.");
+       System.out.println("Student "+std2.stdName+" got "+std2.marks+" marks.");
     }
 }
 
-public class Student{
+class Student{
     String stdName;
     int marks;
 
