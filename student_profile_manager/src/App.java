effe036ce3d1859703e37e78d48e38311b82d54d
Aashika <khatiwadaaashika12@gmail.com>
2026-01-27 10:58:48 +0545

    java

--- student_profile_manager/src/App.java
+++ student_profile_manager/src/App.java
@@ -1,9 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
     }
 }
 
 public class Student{
-    
+    String stdName;
+    int marks;
+
+    Student(String sName,int marks){
+        stdName=sName;
+        this.marks=marks;
+    }
+    // copy constructor
+    Student(Student stdObj){
+        stdName=stdObj.stdName;
+        marks=stdObj.marks;
+    }
 }
