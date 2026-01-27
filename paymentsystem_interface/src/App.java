8ce9072d994f64520054039d00144663d4ee28f9
Aashika <khatiwadaaashika12@gmail.com>
2026-01-27 18:46:58 +0545

    interface

--- paymentsystem_interface/src/App.java
+++ paymentsystem_interface/src/App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Payment p;
+        p= new Esewa();
+        p.pay(500);
+        p=new Khalti();
+        p.pay(7000);
+        p=new Cash();
+        p.pay(20000);
     }
 }
 interface Payment{
@@ -8,16 +14,16 @@ interface Payment{
  }
 class Esewa implements Payment{
     public void pay(double amt){
-        System.out.println("User payed Rs."+amt+" using Esewa");
+        System.out.println("User payed Rs."+amt+" using Esewa.");
     }
 }
 class Khalti implements Payment{
     public void pay(double amt){
-        System.out.println("User payed Rs."+amt+" using Khalti");
+        System.out.println("User payed Rs."+amt+" using Khalti.");
     }
 }
 class Cash implements Payment{
     public void pay(double amt){
-        System.out.println("User payed Rs."+amt+" using Khalti");
+        System.out.println("User payed Rs."+amt+" in cash.");
     }
 }
