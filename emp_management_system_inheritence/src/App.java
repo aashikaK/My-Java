a104db5b110c56c4b2d52de9249c16959bc0420b
Aashika <khatiwadaaashika12@gmail.com>
2026-01-27 14:13:35 +0545

    inheritance

--- emp_management_system_inheritence/src/App.java
+++ emp_management_system_inheritence/src/App.java
@@ -1,5 +1,43 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+    }
+}
+class Employee{
+    String eName;
+    int eId;
+    double salary;
+
+    Employee(String ename,int eId,double salary){
+        eName=ename;
+        this.eId=eId;
+        this.salary=salary;
+    }
+    public void displayInfo(){
+        System.out.println("The name of employee with emp id: "+eId+" is: "+eName+" and salary is:"+salary);
+    }
+}
+class FullTimeEmp extends Employee{
+    double bonus;
+    FullTimeEmp(int eId,String eName,double salary,double bonus){
+        super(eName, eId, salary);
+        this.bonus=bonus;
+    }
+    public void displayInfo(){
+        double totalSalary= salary+bonus;
+        System.out.println("The total salary of full time worker is: "+totalSalary);
+    }
+}
+class PartTimeEmp extends Employee{
+    int workHours;
+    double ratePerHr;
+    PartTimeEmp(int eId,String eName,double salary,double ratePerHr,int workHours){
+        super(eName, eId, salary);
+        this.ratePerHr=ratePerHr;
+        this.workHours=workHours;
+    }
+    public void displayInfo(){
+        double totalSalary= workHours*ratePerHr;
+        System.out.println("The total salary of part time worker is: "+totalSalary);
     }
 }
