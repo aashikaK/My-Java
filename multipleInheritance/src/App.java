246c26951d2ac80acad5f6b09cc270f8a2d9d6d6
Aashika <khatiwadaaashika12@gmail.com>
2026-01-27 14:34:53 +0545

    multiple interface

---
+++ multipleInheritance/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+    }
+}
+interface A{
+    void show(); //its abstract+public by default
+}
+interface B{
+    void show(); 
+}
+class Multiple implements A,B{
+    public void show()// method of classes are defauy by default so write public  while implementing interfae
+    {
+        System.out.println("Interface A and B");
+    }
+}
