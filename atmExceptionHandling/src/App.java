3977dd11d0ac4c5fd35520fcfcda8f360acbd5bd
Aashika <khatiwadaaashika12@gmail.com>
2026-01-29 11:20:50 +0545

    exception

--- atmExceptionHandling/src/App.java
+++ atmExceptionHandling/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ATM obj= new ATM();
+        obj.withdrawl(1100);
     }
 }
 
@@ -11,7 +12,7 @@ class InsufficientBalanceException extends RuntimeException{
 }
 class ATM {
     int balance=10000;
-    void withdrawl(int amount) extends InsufficientBalanceException{
+    void withdrawl(int amount) throws InsufficientBalanceException{
 
         if (amount<=0){
             throw new IllegalArgumentException();
@@ -19,5 +20,8 @@ class ATM {
         if(amount>balance){
             throw new InsufficientBalanceException("Insufficient Balance");
         }
+        balance = balance - amount;
+        System.out.println("Withdrawal successful");
+        System.out.println("Remaining balance: " + balance);
     }
 }
