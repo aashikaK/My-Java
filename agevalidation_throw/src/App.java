b068c6df55988515c4f722d232b01d5d9df0479e
Aashika <khatiwadaaashika12@gmail.com>
2026-01-29 10:30:35 +0545

    throw

--- agevalidation_throw/src/App.java
+++ agevalidation_throw/src/App.java
@@ -1,10 +1,11 @@
 public class App {
-    double withdraw=6000;
-    double total_balance=25000;
+    static double withdraw=60000;
+    static double total_balance=25000;
     public static void main(String[] args) throws Exception {
         if(withdraw>total_balance){
-            throw new InsufficientBalanceException("Insufficient Balance!")
+            throw new InsufficientBalanceException("Insufficient Balance!");
         }
+        System.out.println("Withdrawl sucessful.");
     }
 }
 class InsufficientBalanceException extends RuntimeException{
