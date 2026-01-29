34a407841e6b59574a99c439232b65446c578b9c
Aashika <khatiwadaaashika12@gmail.com>
2026-01-29 11:01:22 +0545

    throws

--- filereadingmethod_throws/src/App.java
+++ filereadingmethod_throws/src/App.java
@@ -1,17 +1,24 @@
 import java.io.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-
-class RreadingFile{
+    
     static void readFile() throws IOException{
-        FileReader fr = new FileReader(text.txt);
-        fr.read();
+        FileReader fr = new FileReader("text.txt");
+       System.out.println(fr.read()); 
+       System.out.println((char)fr.read()); 
+       int ch;
+       while((ch=fr.read())!=-1){
+        System.out.println((char)fr.read());
+       }
     }
     static void processFile() throws IOException{
         readFile();
     }
+    public static void main(String[] args) throws Exception {
+        try{
+            processFile();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
 }
+    }}
